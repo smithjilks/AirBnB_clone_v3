34e08999f5ad48ef24be3fa39be638d1fbdccd20
smithjilks <smithjilks@gmail.com>
2022-02-05 21:41:46 +0300

    Add app.py

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Starts a flask api"""
+
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from models import storage
+import os
+
+
+app = Flask(__name__)
+CORS(app, resources={"/api/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
+CORS(app, resources={"/api/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """ removes the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    if os.getenv("HBNB_API_HOST") and os.getenv("HBNB_API_PORT"):
+        app.run(host=os.getenv("HBNB_API_HOST"),
+                port=os.getenv("HBNB_API_PORT"), threaded=True)
+    else:
+        app.run(host='0.0.0.0', port=5000, threaded=True)
